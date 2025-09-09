fd6a44889739654206a0d31015eb375e9b0fc8e4
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2025-09-09 21:33:28 +0100

    day 014 progress

---
+++ day 014/main.py
@@ -0,0 +1,51 @@
+import random
+import sys
+import os
+import art
+import game_data
+
+lost = False
+first_round = True
+score = 0
+
+def check_decision(player_choice, followers_a, followers_b):
+    """take player coice and the two prospect's number of followers and return if player is right as a boolean"""
+    if player_choice == "a":
+        if followers_a > followers_b:
+            return True
+        elif followers_a == followers_b:
+            return True
+        else:
+            return False
+    else:
+        if followers_b > followers_a:
+            return True
+        elif followers_b == followers_a:
+            return True
+        else:
+            return False
+
+if len(game_data.data) == 1:
+    sys.exit("error: not enough data in game data bank")
+prospect_a = random.choice(game_data.data)
+prospect_b = prospect_a
+while prospect_a == prospect_b:
+    prospect_b = random.choice(game_data.data)
+
+while not lost:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if not first_round:
+        print(f"You're right! Current score: {score}")
+    print(art.logo)
+    print(f"Compare A: {prospect_a['name']}, a {prospect_a['description']}, from {prospect_a['country']}.")
+    print(art.vs)
+    print(f"Against B: {prospect_b['name']}, a {prospect_b['description']}, from {prospect_b['country']}.")
+    valid_input = False
+    while not valid_input:
+        player_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+        if player_choice in ["a"]
+    if check_decision(player_choice, prospect_a["follower_count"], prospect_b["follower_count"]):
+        score += 1
+    elif check_decision:
+        break
+print("Sorry, that's wrong. Final score: ")
