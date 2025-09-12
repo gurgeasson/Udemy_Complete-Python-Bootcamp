74392aa3fca9c17e49555278e1320b26bb54f6c1
gurgeasson <hatizsak@gmail.com>
2025-09-12 21:38:41 +0100

    improvements

--- day 014/main.py
+++ day 014/main.py
@@ -4,9 +4,10 @@ import os
 import art
 import game_data
 
-lost = False
-first_round = True
 score = 0
+
+if len(game_data.data) == 1:
+    sys.exit("error: not enough data in game data bank")
 
 def check_decision(player_choice, followers_a, followers_b):
     """take player coice and the two prospect's number of followers and return if player is right as a boolean"""
@@ -25,17 +26,20 @@ def check_decision(player_choice, followers_a, followers_b):
         else:
             return False
 
-if len(game_data.data) == 1:
-    sys.exit("error: not enough data in game data bank")
-prospect_a = random.choice(game_data.data)
-prospect_b = prospect_a
-while prospect_a == prospect_b:
-    prospect_b = random.choice(game_data.data)
+def pick_random_prospect_from_list_of_dictionaries(data, a):
+    """takes a list and an item that it can't take from that list to avoid picking same item again, and returns an item from the list"""
+    prospect = {}
+    if a == {}:
+        prospect = random.choice(data)
+    else:
+        while prospect == {} or prospect == a:
+            prospect = random.choice(data)        
+    return prospect
 
-while not lost:
+while True:
+    prospect_a = pick_random_prospect_from_list_of_dictionaries(game_data.data, {})
+    prospect_b = pick_random_prospect_from_list_of_dictionaries(game_data.data, prospect_a)
     os.system('cls' if os.name == 'nt' else 'clear')
-    if not first_round:
-        print(f"You're right! Current score: {score}")
     print(art.logo)
     print(f"Compare A: {prospect_a['name']}, a {prospect_a['description']}, from {prospect_a['country']}.")
     print(art.vs)
@@ -43,9 +47,12 @@ while not lost:
     valid_input = False
     while not valid_input:
         player_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
-        if player_choice in ["a"]
+        if player_choice in ["a", "b"]:
+            valid_input = True
     if check_decision(player_choice, prospect_a["follower_count"], prospect_b["follower_count"]):
         score += 1
-    elif check_decision:
+        print(f"You're right! Current score: {score}")
+        input("to continue, press 'enter'")
+    else:
         break
-print("Sorry, that's wrong. Final score: ")
+print(f"Sorry, that's wrong. Final score: {score}")
